52ff72238e4d0e75e4950ca94162e607649f3afb
Scaniox <alexl.mills3@gmail.com>
2024-01-31 08:24:42 +0000

    can message generation functions

--- tem-firmware.X/main.c
+++ tem-firmware.X/main.c
@@ -41,25 +41,10 @@
     SOFTWARE.
 */
 
-#include "mcc_generated_files/mcc.h"
+#include "config.h"
 
-volatile uint8_t count = 0;
+int8_t temps[THERM_COUNT];
 
-void CAN_RX_ISR()
-{
-    CAN_MSG_OBJ rx_msg;
-    CAN1_ReceiveFrom(FIFO1, &rx_msg);
-    
-    if(rx_msg.data[0] == 0x42)
-    {
-        count = 0;
-    }
-}
-
-
-/*
-                         Main application
- */
 void main(void)
 {
 
@@ -67,24 +52,15 @@ void main(void)
     
     // Initialize the device
     SYSTEM_Initialize();
-    CAN1_SetFIFO1FullHandler(&CAN_RX_ISR);
-
-    // If using interrupts in PIC18 High/Low Priority Mode you need to enable the Global High and Low Interrupts
-    // If using interrupts in PIC Mid-Range Compatibility Mode you need to enable the Global Interrupts
-    // Use the following macros to:
 
     // Enable the Global Interrupts
     INTERRUPT_GlobalInterruptEnable();
 
-    // Disable the Global Interrupts
-    //INTERRUPT_GlobalInterruptDisable();
-
     while (1)
-    {
-        // Add your application code
-        __delay_ms(1000);        
-
+    {   
+        //read temps
         
+        // set up can interface
         if(CAN_CONFIGURATION_MODE == CAN1_OperationModeGet())
         { 
             CAN1_OperationModeSet(CAN_NORMAL_2_0_MODE);
@@ -92,26 +68,17 @@ void main(void)
 
         if(CAN_NORMAL_2_0_MODE == CAN1_OperationModeGet())
         {
-            msg.msgId = 0x00001;
-            msg.field.formatType = CAN_2_0_FORMAT;
-            msg.field.brs = CAN_NON_BRS_MODE;
-            msg.field.dlc = DLC_1;
-            msg.field.frameType = CAN_FRAME_DATA;
-            msg.field.idType = CAN_FRAME_EXT;
-            msg.data = &count;
 
             if(CAN_TX_FIFO_AVAILABLE == (CAN1_TransmitFIFOStatusGet(CAN1_TX_TXQ)))
             {
-                IO_RC6_SetHigh();
                 CAN1_Transmit(CAN1_TX_TXQ, &msg);
 
                 while(CAN1_TransmitFIFOStatusGet(CAN1_TX_TXQ) == CAN_TX_FIFO_FULL);
 
-                IO_RC6_SetLow();
             }
         }
         
-        count++;
+        __delay_ms(100);   
     }
 }
 /**
